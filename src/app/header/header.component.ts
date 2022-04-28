40bbc4c4c52e43d3e31142fde527b3ea54f2bc45
Greyson-Sawyer <showmethemonkey2@gmail.com>
2022-04-28 16:09:26 -0700

    2 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { DateSelectionService } from '../services/date-selection.service';
 
 @Component({
@@ -7,6 +7,14 @@ import { DateSelectionService } from '../services/date-selection.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('style.backgroundColor') get backgroundColor() {
+    return this.dateSelectionService.isTheSelectedDateTodaysDate
+      ? this.highlightColor
+      : this.defaultColor;
+  }
+
+  highlightColor = 'rgb(56, 127, 129);';
+  defaultColor = '#fcfdfd';
   constructor(public dateSelectionService: DateSelectionService) {}
 
   ngOnInit() {}
@@ -14,8 +22,6 @@ export class HeaderComponent implements OnInit {
   get selectedDate() {
     return this.dateSelectionService.selectedDate;
   }
-
-  
 
   addDays(numberOfDays: number) {
     this.dateSelectionService.addDays(numberOfDays);
